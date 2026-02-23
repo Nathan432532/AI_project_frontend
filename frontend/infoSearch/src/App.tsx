59e11ce4530acd20850876e4956be55dfa1315fb
Nathan ter Hark <s159751@ap.be>
2026-02-23 18:57:13 +0100

    Added SearchPage frontend

---
+++ frontend/infoSearch/src/App.tsx
@@ -0,0 +1,7 @@
+import SearchPage from "./pages/SearchPage";
+
+function App() {
+  return <SearchPage userName="Jan Janssen"/>
+}
+
+export default App;
---
+++ frontend/infoSearch/src/api/client.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+});
+
+client.interceptors.request.use(async (config) => {
+    const token = await getAccessToken();
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
